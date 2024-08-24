768e8f373a8853651619587998b6c87cb7e73014
Thanh Tùng <electricthanhtung@gmail.com>
2024-08-24 21:39:02 +0700

    Update example for ESP GPIO

--- Example/EspIO/Pin/Src/main.java
+++ Example/EspIO/Pin/Src/main.java
@@ -3,6 +3,7 @@ import machine.gpio.Pin;
 
 public class main {
     public static void main(String[] args) throws Exception {
+        System.out.println("Hello form ESP32");
         Pin.setMode(15, Pin.OUTPUT);
         while(true) {
             Pin.writePin(15, true);
