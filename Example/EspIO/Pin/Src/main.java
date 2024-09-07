33a508026b99f6594cc438a30e2f9c92c914dd0f
Thanh Tùng <electricthanhtung@gmail.com>
2024-09-07 09:51:09 +0700

    Update FlintUartServer tool

--- Example/EspIO/Pin/Src/main.java
+++ Example/EspIO/Pin/Src/main.java
@@ -5,11 +5,13 @@ public class main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello form ESP32");
         Pin.setMode(15, Pin.OUTPUT);
+        long a = 10;
+        double d = 31234124.5;
         while(true) {
             Pin.writePin(15, true);
-            Thread.sleep(100);
+            Thread.sleep(200);
             Pin.writePin(15, false);
-            Thread.sleep(100);
+            Thread.sleep(200);
         }
     }
 }
