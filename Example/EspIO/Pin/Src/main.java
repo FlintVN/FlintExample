d2d175ef6bfec34fc94d9b0657e2c59bdbe20c98
Thanh Tùng <electricthanhtung@gmail.com>
2024-11-15 21:06:58 +0700

    Update example code for Pin

--- Example/EspIO/Pin/Src/main.java
+++ Example/EspIO/Pin/Src/main.java
@@ -5,8 +5,6 @@ public class main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello form ESP32");
         Pin.setMode(15, Pin.OUTPUT);
-        long a = 10;
-        double d = 31234124.5;
         while(true) {
             Pin.writePin(15, true);
             Thread.sleep(200);
