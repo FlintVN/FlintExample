c4875500541abf1f1848234ae6fe308dd99222b5
Thanh Tùng <electricthanhtung@gmail.com>
2025-11-26 23:26:02 +0700

    Add example for SpiMaster

---
+++ examples/FlintIO/SpiMaster/src/Main.java
@@ -0,0 +1,40 @@
+
+import flint.machine.*;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello form ESP32");
+
+        SpiMaster spiMaster = new SpiMaster("SPI2", 8000000);
+
+        // Set SPI speed
+        // spiMaster.setSpeed(8000000);
+
+        // If the SPI pins are not set, the default values ​​will be used (or it may be not set).
+        // spiMaster.setMisoPin(12);
+        // spiMaster.setMosiPin(13);
+        // spiMaster.setClkPin(14);
+
+        spiMaster.open();
+
+        // Write a byte
+        spiMaster.write(0x55);
+
+        // Read a byte
+        int b = spiMaster.read();
+
+        byte[] tx = new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        byte[] rx = new byte[10];
+
+        // Write a buffer
+        spiMaster.write(tx);
+
+        // Read a buffer
+        int size = spiMaster.read(rx);
+
+        // Read and write
+        size = spiMaster.readWrite(tx, rx, 10);
+    }
+}
