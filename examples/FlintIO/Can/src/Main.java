b6998ecd33d7889cb7ec85fd0022bdf0d13e5993
MeoU_Ne <ducson9112k@gmail.com>
2025-12-30 14:44:46 +0700

    Update example for CAN

---
+++ examples/FlintIO/Can/src/Main.java
@@ -0,0 +1,29 @@
+import flint.machine.Can;
+import java.util.Arrays;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Can can = new Can("CAN-SelfTest", 1, 500_000, 21, 22);
+            can.open();
+            can.start();
+
+            byte[] payload = {0x11, 0x22, 0x33, 0x44};
+            Can.CanMessage txMsg = Can.CanMessage.of(0x123, payload);
+            can.writeMessage(txMsg, 1000);
+
+            Can.CanMessage rxMsg = can.receiveMessage(1000);
+            if (rxMsg != null && Arrays.equals(txMsg.data, rxMsg.data)) {
+                System.out.println("Self-test PASSED");
+            } else {
+                System.out.println("Self-test FAILED");
+            }
+
+            can.stop();
+            can.close();
+        } catch (IOException e) {
+            /* Nothing TODO */
+        }
+    }
+}
