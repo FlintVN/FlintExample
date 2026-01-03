1d497be1496443cd1fbbd0f777bdbec705169beb
MeoU_Ne <ducson9112k@gmail.com>
2026-01-03 15:38:40 +0700

    Update Example

--- examples/FlintIO/Can/src/Main.java
+++ examples/FlintIO/Can/src/Main.java
@@ -1,29 +1,53 @@
 import flint.machine.Can;
+import flint.machine.CanMessage;
+import flint.machine.CanStatus;
+import flint.machine.Can.CanMode;  // Thêm import này
+
 import java.util.Arrays;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Can can = new Can("CAN", 1, 500_000).setTxPin(21).setRxPin(22).setSamplePoint(75);
-            can.open();
-            can.start();
+    public static void main(String[] args) throws Exception {
+        // CAN0 – main bus
+        Can can0 = new Can("CAN0", 0, 500_000)
+                .setTxPin(21)
+                .setRxPin(22)
+                .setMode(CanMode.NORMAL);
 
-            byte[] payload = {0x11, 0x22, 0x33, 0x44};
-            Can.CanMessage txMsg = Can.CanMessage.of(0x123, payload);
-            can.writeMessage(txMsg, 1000);
+        // CAN1 – sniffer / debug
+        Can can1 = new Can("CAN1", 1, 250_000)
+                .setTxPin(25)
+                .setRxPin(26)
+                .setMode(CanMode.LISTEN_ONLY);
 
-            Can.CanMessage rxMsg = can.receiveMessage(1000);
-            if (rxMsg != null && Arrays.equals(txMsg.data, rxMsg.data)) {
-                System.out.println("Self-test PASSED");
-            } else {
-                System.out.println("Self-test FAILED");
-            }
+        can0.open();
+        // can1.open(); // Not supported on some platforms
 
-            can.stop();
-            can.close();
-        } catch (IOException e) {
-            /* Nothing TODO */
+        can0.start();
+        // can1.start(); // Not supported on some platforms
+
+        // Send
+        can0.writeMessage(
+            CanMessage.of(0x123, new byte[]{0x11,0x22}),
+            1000
+        );
+
+        // Receive on sniffer
+        CanMessage rx = can1.receiveMessage(1000);
+        if (rx != null) {
+            System.out.println("Sniffed: " + rx);
         }
+
+        // Check status
+        CanStatus status = can0.getStatus();
+        if (status != null) {
+            System.out.println("Status: " + status);
+        }
+
+        can0.stop();
+        // can1.stop(); // Not supported on some platforms
+
+        can0.close();
+        // can1.close(); // Not supported on some platforms
     }
 }
